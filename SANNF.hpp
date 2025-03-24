af8e041f2b0f2a2daeec44ffeca8d0eeea464869
trololoxa <nikolai.2006-2015@yandex.ru>
2025-03-25 02:38:02 +0300

    Initial commit

---
+++ SANNF.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace SANNF
+{
+    //TODO: Get all processing steps
+    class NeuralNetwork
+    {
+        //TODO: storing different size layers, weights
+        //TODO: getting result of NN and back propagation
+    };
+    
+    class Layer
+    {
+        //TODO: basic neuron amount setting, activation type and just layer types
+    };
+    
+    class Optimizer
+    {
+    
+    };
+}
